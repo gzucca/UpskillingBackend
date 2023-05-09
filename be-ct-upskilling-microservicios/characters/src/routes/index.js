9d4fa81e7264fda73c351ead9aaa24ca6539ab43
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-05-09 22:41:55 +0200

    Routes and controllers directory

--- be-ct-upskilling-microservicios/characters/src/routes/index.js
+++ be-ct-upskilling-microservicios/characters/src/routes/index.js
@@ -4,5 +4,6 @@ const controllers = require('../controllers');
 const router = Router();
 
 router.get("/", controllers.getCharacters)
+router.post("/", controllers.createCharacter)
 
 module.exports = router;
