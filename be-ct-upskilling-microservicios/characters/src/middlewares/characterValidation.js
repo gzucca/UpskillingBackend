5fd32a83ecdf54fc36d952f1e33f380924eb9317
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-05-11 14:20:31 +0200

    Error handling with statusCode and Middleware validator characters service

--- be-ct-upskilling-microservicios/characters/src/middlewares/characterValidation.js
+++ be-ct-upskilling-microservicios/characters/src/middlewares/characterValidation.js
@@ -1,5 +1,7 @@
+const  {ClientError} = require("../utils/errors");	
+
 module.exports = (req, res, next) => {
   const { name } = req.body;
   if (name) return next();
-  else throw Error("Name is required");
+  else throw new ClientError("Error in name", 401);
 };
