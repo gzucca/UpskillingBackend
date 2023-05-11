9f907f3e4d553bee276efe213c34923d4dc5b6df
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-05-11 17:43:38 +0200

    added gitignore rules

---
+++ be-ct-upskilling-microservicios/star_wars_api_microservices/films/src/data/index.js
@@ -0,0 +1,13 @@
+const films = require("./films.json");
+
+//This json is going to be replaced with an API later. However, the rest of the service will remain the same, as this functions mimics the actual functioning of an API.
+
+module.exports = {
+  list: async () => {
+    return films;
+  },
+
+  create: async () => {
+    throw Error("Hay un error en la Base de Datos al momento de crear un film");
+  },
+};
