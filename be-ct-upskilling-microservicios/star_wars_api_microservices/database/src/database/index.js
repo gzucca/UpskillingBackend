4ac03c84a1a62dacdaa59187c58ecd51679824d2
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-05-17 00:18:04 +0200

    getter function in character schema

--- be-ct-upskilling-microservicios/star_wars_api_microservices/database/src/database/index.js
+++ be-ct-upskilling-microservicios/star_wars_api_microservices/database/src/database/index.js
@@ -3,6 +3,8 @@ const { MONGO_URI } = require("../config/envs");
 
 const conn = mongoose.createConnection(MONGO_URI);
 
+
+
 module.exports = {
   Character: conn.model("Character", require("./schemas/characterSchema")),
   Film: conn.model("Film", require("./schemas/filmSchema")),
